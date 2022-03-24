5f9f67127619b908ba78e5c15582f92bcfb7c411
kxweb <75758420+kxweb@users.noreply.github.com>
2022-03-24 17:19:49 +0300

    optimise perfomance
    $ time cat /proc/net/tcp /proc/net/tcp6 | awk '{print $4}' 2>/dev/null| grep -v st | sort | uniq -c | sed -e s/' 0'/' '/g -e s/'A'/'10'/g -e s/'B'/'11'/g | awk '{print $2":"$1}'
    1:46874
    6:50
    10:26
    
    real    0m0.207s
    user    0m0.051s
    sys     0m0.199s
    
    time awk '{print $4}' /proc/net/tcp /proc/net/tcp6  2>/dev/null| grep -v st | sort | uniq -c | sed -e s/' 0'/' '/g -e s/'A'/'10'/g -e s/'B'/'11'/g | awk '{print $2":"$1}'
    1:46899
    6:70
    10:26
    
    real    0m17.344s
    user    0m0.073s
    sys     0m17.273

--- json_item_tcp.sh
+++ json_item_tcp.sh
@@ -2,7 +2,7 @@
 # for Linux
 
 STATS=(UNKNOWN:0 ESTABLISHED:0 SYN_SENT:0 SYN_RECV:0 FIN_WAIT1:0 FIN_WAIT2:0 TIME_WAIT:0 CLOSED:0 CLOSE_WAIT:0 LAST_ACK:0 LISTEN:0 CLOSING:0)
-CONN="$(awk '{print $4}' /proc/net/tcp /proc/net/tcp6 | grep -v st | sort | uniq -c | sed -e s/' 0'/' '/g -e s/'A'/'10'/g -e s/'B'/'11'/g | awk '{print $2":"$1}')"
+CONN="$(cat /proc/net/tcp /proc/net/tcp6 | awk '{print $4}' | grep -v st | sort | uniq -c | sed -e s/' 0'/' '/g -e s/'A'/'10'/g -e s/'B'/'11'/g | awk '{print $2":"$1}')"
 
 if [ ! -z "$CONN" ];then
     for s in $CONN
